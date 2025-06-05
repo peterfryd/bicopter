02c809bc496d8d060fa53e67752a25d7c009fe72
Anderslind <32484749+anders-lind@users.noreply.github.com>
2025-06-05 12:44:20 +0200

    Newest version

--- mini_project_par.m
+++ mini_project_par.m
@@ -1,12 +1,22 @@
 clc, clear close all
-% Quadcopter parameters, taken from Lecture 2, question 1
-% Jerome Jouffroy, February 2024
 
-m = 4; % mass in kg
-g = 9.81; % gravity constant
+m = 2; % Mass in kg
+g = 9.81; % Gravity constant
 
-% inertia matrix
+L = 0.5;  % Spacing along x-axis (full length is 2L)
+D = 0.2;  % Height from rotor to CoM
+
+% Inertia matrix
 Ic = diag([ 2 ; 2 ; 4 ]);
 
-% rigid-body mass matrix
+% Rigid-body mass matrix
 M_RB = [ m*eye(3) , zeros(3) ; zeros(3) , Ic ];
+
+
+% Limits in motor thrusts (N) and servo angles (rad)
+% Note that upwards thrust is negative (NED convention)
+delta_limits = [-pi/3; pi/3];
+T_limits = [-1000, 0];
+
+% List of params for Simulink
+params = [m, g, L, D, delta_limits(1), delta_limits(2), T_limits(1), T_limits(2)];
